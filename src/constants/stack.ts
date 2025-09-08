cc40574877ddc54c54683ab425be10957b180e35
Élcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-09-08 17:49:14 -0300

    feat: Added stack constants for major languages ​​and frameworks

---
+++ src/constants/stack.ts
@@ -0,0 +1,75 @@
+export const stack = [
+  {
+    stackname: "React.js",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+    reference: "https://reactjs.org/",
+  },
+  {
+    stackname: "Tailwind CSS",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+    reference: "https://tailwindcss.com/",
+  },
+  {
+    stackname: "TypeScript",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg",
+    reference: "https://www.typescriptlang.org/",
+  },
+  {
+    stackname: "Next.js",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    reference: "https://nextjs.org/",
+  },
+  {
+    stackname: "JavaScript",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+    reference: "https://www.javascript.com/",
+  },
+
+  {
+    stackname: "Node.js",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
+    reference: "https://nodejs.org/",
+  },
+  {
+    stackname: "SolidJS",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/solidjs/solidjs-original.svg",
+    reference: "https://solidjs.com/",
+  },
+  {
+    stackname: "Java",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
+    reference: "https://www.java.com/",
+  },
+  {
+    stackname: "Python",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+    reference: "https://www.python.org/",
+  },
+  {
+    stackname: "Fastapi",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg",
+    reference: "https://fastapi.tiangolo.com/",
+  },
+  {
+    stackname: "Spring Boot",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg",
+    reference: "https://spring.io/",
+  },
+  {
+    stackname: "Docker",
+    stackicon:
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
+    reference: "https://www.docker.com/",
+  },
+];
