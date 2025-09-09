899db54a6b2e1e6cdefe4874eb70db1196a53da5
Élcio M. Fernandes <elcio@elcio.dev>
2025-09-09 19:47:08 -0300

    chore: Updated index.ts, component export centralizer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { Nav } from "./Nav";
+export { Button } from "./Button";
 export { Marquee } from "./Marquee";
 export { SnapProvider, SnapSection } from "./Snap";
+export { BeamTechnologies } from "./BeamTechnologies";
+export { AnimatedThemeToggler } from "./AnimatedThemeToggler";
