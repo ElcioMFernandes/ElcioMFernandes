9efe6c2dce79bd36ad2f7898e7df510d61aceff2
ElcioMFernandes <elcio@elcio.dev>
2025-09-09 23:18:23 -0300

    fix: Margin adjustment on mobile devices, as it was too large, which ends up hiding the pagination

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -73,7 +73,7 @@ export const Projects = () => {
           : data.map((repo) => <RepositoryCard key={repo.id} repo={repo} />)}
       </div>
       {total > 1 && (
-        <Pagination className="mt-8">
+        <Pagination className="mt-4 sm:mt-8">
           <PaginationContent>
             <PaginationItem>
               <PaginationPrevious
