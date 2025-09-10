7a977c4095c8d6bfa14e2f7a8a20655b48b5c278
ElcioMFernandes <elcio@elcio.dev>
2025-09-09 23:14:22 -0300

    fix: Fixed the custom pointer not being displayed on mobile devices

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import App from "./App.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Pointer } from "./components/Pointer.tsx";
+import { useScreenSize } from "./hooks/useScreenSize.ts";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Pointer />
-    <App />
-  </StrictMode>
-);
+const Root = () => {
+  const screenSize = useScreenSize();
+  const isMobile = screenSize === "sm";
+
+  return (
+    <StrictMode>
+      {!isMobile && <Pointer />}
+      <App />
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(<Root />);
