b93e708ca48faaed74777d541dea0a906e493bcb
ElcioMFernandes <elcio@elcio.dev>
2025-09-09 23:03:48 -0300

    chore: Centralized components export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ export { Nav } from "./Nav";
 export { Button } from "./Button";
 export { Marquee } from "./Marquee";
 export { SnapProvider, SnapSection } from "./Snap";
-export { BeamTechnologies } from "./BeamTechnologies";
 export { AnimatedThemeToggler } from "./AnimatedThemeToggler";
+export { StatCard } from "./StatCard";
+export { GithubStats } from "./GithubStats";
+export { RepositoryCard, RepositoryCardSkeleton } from "./RepositoryCard";
